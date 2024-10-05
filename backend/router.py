b9bf15faf91b7eb4285c0f0416b0d859f0757cb1
Bruno Silva <bruno.ogs@gmail.com>
2024-10-04 23:53:47 -0400

    update

---
+++ backend/router.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, get_db
+from schemas import ProductResponse, ProductUpdate, ProductCreate
+from typing import List
+from crud import (
+    create_product,
+    get_products,
+    get_product,
+    delete_product,
+    update_product,
+)
+
+router = APIRouter()
+
+# criar minha rota de buscar todos os itens
+@router.get("/products/", response_model=List[ProductResponse])
+def read_all_products_route(db: Session = Depends(get_db)):
+    products = get_products(db)
+    return products
+
+# criar minha rota de buscar 1 item
+@router.get("/products/{product_id}", response_model=ProductResponse)
+def read_one_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = get_product(db=db, product_id = product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="voce ta querendo buscar um produto que nao existe")
+    return db_product
+
+
+# criar minha rota de adicionar um item
+@router.post("/products/", response_model=ProductResponse)
+def create_product_route(product: ProductCreate, db: Session = Depends(get_db)):
+    return create_product(db=db, product=product)
+
+# criar minha rota de deletar um item
+@router.delete("/products/{product_id}", response_model=ProductResponse)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    product_db = delete_product(product_id=product_id, db=db)
+    if product_db is None:
+        raise HTTPException(status_code=404, detail="voce ta querendo DELETAR um produto que nao existe")
+    return product_db
+
+# criar minha rota de fazer update nos itens
+@router.put("/products/{product_id}", response_model=ProductResponse)
+def atualizar_product(product_id: int, product: ProductUpdate, db: Session = Depends(get_db)):
+    product_db = update_product(db=db, product_id=product_id, product=product)
+    if product_db is None:
+        raise HTTPException(status_code=404, detail="voce ta querendo fazer um update um produto que nao existe")
+    return product_db
+
